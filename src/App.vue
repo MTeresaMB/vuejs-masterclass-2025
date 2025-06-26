9d67fb8a6b4f99e46ddaa5936f62e3e425c777f9
MTeresaMB <milanesteresa@gmail.com>
2025-06-26 22:00:59 +0200

    refactor: create SidebarLink component to sidebar links

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import TopNavbar from "./components/ui/layout/TopNavbar.vue";
-import AsideBar from "./components/ui/layout/AsideBar.vue";
+import SideBar from "./components/ui/layout/SideBar.vue";
 </script>
 
 <template>
-  <AsideBar />
+  <SideBar />
   <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
     <TopNavbar />
     <main>
