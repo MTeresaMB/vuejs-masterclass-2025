6862f2e3a4c324d3eca19813c5761f69c4db6488
MTeresaMB <milanesteresa@gmail.com>
2025-06-26 19:16:46 +0200

    feat: add input component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { BaseInput } from "./components/ui/input";
+</script>
 
 <template>
   <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
     <form class="w-full max-w-96">
-      <input type="text" placeholder="Search..." class="w-full pl-8 bg-background" />
+      <BaseInput type="text" placeholder="Search..." class="w-full pl-8 bg-background" />
     </form>
     <div class="w-8 h-8 rounded-full bg-white"></div>
   </nav>
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-import tailwind from 'tailwindcss';
+import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer';
 
 // https://vite.dev/config/
@@ -10,7 +10,7 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        tailwind(),
+        tailwindcss(),
         autoprefixer(),
       ],
     },
