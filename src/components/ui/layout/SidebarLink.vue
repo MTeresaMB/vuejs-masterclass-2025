389f29b4882a7079332526d8fe6547d3c3fef8e4
MTeresaMB <milanesteresa@gmail.com>
2025-06-27 19:15:04 +0200

    style: configure vue router active links with tailwindcss

--- src/components/ui/layout/SidebarLink.vue
+++ src/components/ui/layout/SidebarLink.vue
@@ -12,6 +12,7 @@ defineProps<{
 
 <template>
   <RouterLink v-for="link in links" :key="link.title" :to="link.to"
+    exact-active-class="bg-primary text-primary-foreground dark:bg-primary/80 dark:text-primary-foreground"
     class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground">
     <iconify-icon :icon="link.icon"></iconify-icon>
     <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
