d85dea1ec9f4b259942f86281a215f2052d226d6
MTeresaMB <milanesteresa@gmail.com>
2025-06-29 13:27:34 +0200

    feat: add AuthLayout in layout folder

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
-import TopNavbar from "./components/ui/layout/TopNavbar.vue";
-import SideBar from "./components/ui/layout/SideBar.vue";
+import AuthLayout from "./components/layout/main/AuthLayout.vue";
 </script>
 
 <template>
-  <SideBar />
-  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
-    <TopNavbar />
-    <main>
-      <RouterView />
-    </main>
-  </div>
+  <AuthLayout>
+    <RouterView />
+  </AuthLayout>
 </template>
