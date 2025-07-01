62c6fd70857d234cfc0311ab089fa1011d987b2b
MTeresaMB <milanesteresa@gmail.com>
2025-07-01 17:35:25 +0200

    feat: implement unplugin-auto-import and refactor files

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer';
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 // https://vite.dev/config/
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [
-        tailwindcss(),
-        autoprefixer(),
-      ],
+      plugins: [tailwindcss(), autoprefixer()],
     },
   },
   plugins: [
@@ -23,6 +22,26 @@ export default defineConfig({
           isCustomElement: (element) => element.startsWith('iconify-icon'),
         }
       }
+    }),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      imports: ['vue', 'vue-router', VueRouterAutoImports,
+        {
+          'vue-router': [
+            'RouterLink',
+            'RouterView',
+            'useRouter',
+            'useRoute',
+            'useLink'
+          ]
+        }],
+      dts: true,
+      viteOptimizeDeps: true
     }),
   ],
   resolve: {
